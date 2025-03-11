1b1ff705490f0b2d411d677e41a0a16c989b5d87
imandaidf <id.imanda.f@gmail.com>
2025-03-11 13:53:15 +0700

    Add About Screen

---
+++ app/(tabs)/AboutScreen.tsx
@@ -0,0 +1,20 @@
+import { Text, View, StyleSheet } from 'react-native';
+
+export default function AboutScreen() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>About Screen</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 24,
+  },
+});
